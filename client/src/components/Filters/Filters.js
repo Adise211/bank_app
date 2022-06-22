ab09ac7e69162e0e67e38f72ae075e115a8bcff5
Adise211 <adisemamo211@gemail.com>
2022-06-22 11:12:37 +0300

    Filters(new) component

---
+++ client/src/components/Filters/Filters.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+
+
+const Filters = () => {
+  return (
+    <div className='container'>
+      <h3>Filters:</h3>
+      <p>1.Balance</p>
+      <p>2.City</p>
+      <p>3.Mortgage</p>
+      <p>4.of cards</p>
+    </div>
+  )
+}
+
+export default Filters;
