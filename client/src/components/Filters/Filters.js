b038d349f1add7f058a1b8e07727a17ce7f1282a
Adise211 <adisemamo211@gemail.com>
2022-06-27 09:02:17 +0300

    more style

--- client/src/components/Filters/Filters.js
+++ client/src/components/Filters/Filters.js
@@ -4,6 +4,7 @@ import './style.css';
 import { useDispatch, useSelector} from 'react-redux';
 import { TextField, Button } from '@material-ui/core';
 import { balanceFilter, cityFilter, mortgageFilter, cardsFilter, twoFilters, threeFilters } from '../../actions/filters';
+import { getPosts } from  '../../actions/posts';
 
 
 
@@ -17,6 +18,7 @@ const Filters = () => {
   const [ list, setList ] = useState([]);
   const [ selected] = useState('');
   const posts = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
 
 
   const addCity = (e) => {
@@ -27,14 +29,10 @@ const Filters = () => {
       } else {
         setList([...list,e.target.value])
       }
-  }
+  };
 
 
 
-
-
-
-  const dispatch = useDispatch();
 
 
   const handleSubmit = (e) => {
@@ -58,25 +56,39 @@ const Filters = () => {
       } else {
         dispatch(twoFilters(start,end,radioValue,cards))
       }
+  };
 
 
+  const clearFilters = () => {
+    setStart('')
+    setEnd('')
+    setCards(null)
+    setRadioValue('None')
+    setList([])
+    dispatch(getPosts())
 
-  }
+  };
 
 
 
 
   return (
     <div className='container'>
-      <h3>Filters</h3>
-        <form>
-          <div className='b-balance'>
-            <p>Balance:</p>
-            <TextField sx={{m:1}} type='number' label='From' color='secondary' variant='filled' size="small" onChange={(e) => setStart(e.target.value)}/>
-            <TextField sx={{m:1}} type='number' label='Up to' color='secondary' variant='filled' size="small" onChange={(e) => setEnd(e.target.value)}/>
-          </div>
+      <p className='title'>Filters</p>
+        <form onSubmit={handleSubmit}>
 
           <div className='c-dropdown-list'>
+            <div className='c-list'>
+                <p>City:</p>
+                <select onChange={addCity} multiple>
+                  <option></option>
+                  {
+                    posts.map(item => {
+                      return  <option key={item._id}>{item.city}</option>
+                    })
+                  }
+                </select>
+            </div>
             <div className='c-selected'>
               <p>{selected}</p>
               {
@@ -87,16 +99,14 @@ const Filters = () => {
                 : []
               }
             </div>
-            <div className='c-list'>
-                <select onChange={addCity} multiple>
-                  <option></option>
-                  {
-                    posts.map(item => {
-                      return  <option key={item._id}>{item.city}</option>
-                    })
-                  }
-                </select>
-            </div>
+          </div><hr/>
+
+
+          <div className='b-balance'>
+            <h4>OR</h4>
+            <p>Balance:</p>
+            <TextField style={{m:1,margin:4}} type='number' label='From' color='primary' variant='filled' size="small" onChange={(e) => setStart(e.target.value)}/>
+            <TextField style={{m:1}} type='number' label='Up to' color='primary' variant='filled' size="small" onChange={(e) => setEnd(e.target.value)}/>
           </div>
 
 
@@ -125,11 +135,12 @@ const Filters = () => {
             </div>
 
             <div className='c-credits'>
+              <p># Of Cards:</p>
               <TextField
                 sx={{m:1}}
                 type='number'
                 label='Credit Cards'
-                color='secondary'
+                color='primary'
                 variant='filled'
                 size="small"
                 onChange={(e) => setCards(e.target.value)}
@@ -139,11 +150,22 @@ const Filters = () => {
           <Button
               type='submit'
               variant="contained"
-              onClick={handleSubmit}
-              color='secondary'>
+              color='primary'
+              style={{margin:10}}>
               Search
           </Button>
+
+          <Button
+              type='button'
+              variant="contained"
+              color='secondary'
+              onClick={clearFilters}
+              style={{}}>
+              Clear Filters
+          </Button>
         </form>
+
+
     </div>
   )
 }
